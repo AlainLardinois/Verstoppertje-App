6c6a15afb90e04908e6fb444895684fefd62cf15
ParrotLync <43622266+ParrotLync@users.noreply.github.com>
2020-06-03 12:51:40 +0200

    Create models

---
+++ Model/UserScore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verstoppertje_App.Model
+{
+    class UserScore
+    {
+        private bool won;
+        private GameRole role;
+        private int score;
+        private User user;
+
+        public UserScore(bool _won, GameRole _role, int _score, User _user)
+        {
+            won = _won;
+            role = _role;
+            score = _score;
+            user = _user;
+        }
+
+        public int Score { get => score;}
+        public bool Won { get => won; }
+        public GameRole Role { get => role; }
+        public User User { get => user;}
+    }
+}
